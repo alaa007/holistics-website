06fd339e1a2d0197928bba8224429e1eb63a8c60
Alaa AH <alaa007@gmail.com>
2026-08-11 12:04:15 +0400

    Add Rubik font support for Arabic locale and update styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,11 @@
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon-32x32.png') }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/apple-touch-icon.png') }}">
+@if (app()->getLocale() === 'ar')
+{{-- Rubik is self-hosted and declared in style.css; preload the Arabic
+     subset so the text does not flash in a fallback face first. --}}
+<link rel="preload" as="font" type="font/woff2" href="{{ asset('fonts/rubik/rubik-arabic.woff2') }}" crossorigin>
+@endif
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 <meta name="theme-color" content="#063b3f">
 @stack('head')
